751bbb1702c414ac98fa2c7ba5c220a75e415947
python-arcade <waatuurawwaa@gmail.com>
2020-11-20 17:54:22 +0100

    Extracted constants. Implemented scrolling, coins, sounds and score.

---
+++ constants.py
@@ -0,0 +1,28 @@
+__author__ = "Bohdan Deineka, Bartlomiej Matuszewski, Bartlomiej Kuzba"
+__copyright__ = "Copyright 2020, The Arcade Project"
+__credits__ = ["Bohdan Deineka", "Bartlomiej Matuszewski", "Bartlomiej Kuzba"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Bohdan Deineka"
+__email__ = "bohdan.deineka@edu.uni.lodz.pl"
+__status__ = "Production"
+
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 2
+PLAYER_JUMP_SPEED = 20
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+LEFT_VIEWPORT_MARGIN = 250
+RIGHT_VIEWPORT_MARGIN = 250
+BOTTOM_VIEWPORT_MARGIN = 50
+TOP_VIEWPORT_MARGIN = 100
--- main.py
+++ main.py
@@ -1,23 +1,20 @@
+__author__ = "Bohdan Deineka, Bartlomiej Matuszewski, Bartlomiej Kuzba"
+__copyright__ = "Copyright 2020, The Arcade Project"
+__credits__ = ["Bohdan Deineka", "Bartlomiej Matuszewski", "Bartlomiej Kuzba"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Bohdan Deineka"
+__email__ = "bohdan.deineka@edu.uni.lodz.pl"
+__status__ = "Production"
+
 import arcade
-
-# Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
-
-CHARACTER_SCALING = 1
-TILE_SCALING = 0.5
-COIN_SCALING = 0.5
-
-PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 2
-PLAYER_JUMP_SPEED = 20
+import constants
 
 
 class Game(arcade.Window):
 
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
 
         self.coin_list = None
         self.wall_list = None
@@ -27,21 +24,37 @@ class Game(arcade.Window):
 
         self.physics_engine = None
 
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Keep track of the score
+        self.score = 0
+
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+
         arcade.set_background_color(arcade.csscolor.BLACK)
 
     def setup(self):
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Keep track of the score
+        self.score = 0
+
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
 
         image_source = "images/character/GraveRobber.png"
-        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
         self.player_sprite.center_x = 24
         self.player_sprite.center_y = 48
         self.player_list.append(self.player_sprite)
 
         for x in range(0, 1250, 24):
-            wall = arcade.Sprite("images/tiles/tile22.png", TILE_SCALING)
+            wall = arcade.Sprite("images/tiles/tile22.png", constants.TILE_SCALING)
             wall.center_x = x
             wall.center_y = 12
             self.wall_list.append(wall)
@@ -54,13 +67,21 @@ class Game(arcade.Window):
                            [768, 31]]
 
         for coordinate in coordinate_list:
-            wall = arcade.Sprite("images/Objects/barrel.png", TILE_SCALING)
+            wall = arcade.Sprite("images/Objects/barrel.png", constants.TILE_SCALING)
             wall.position = coordinate
             self.wall_list.append(wall)
 
+        # Use a loop to place some coins for our character to pick up
+        for x in range(128, 1250, 256):
+            coin = arcade.Sprite(":resources:images/items/coinGold.png", constants.COIN_SCALING)
+            coin.center_x = x
+            coin.center_y = 96
+            self.coin_list.append(coin)
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
                                                              self.wall_list,
-                                                             GRAVITY)
+                                                             constants.GRAVITY)
+
     def on_draw(self):
         arcade.start_render()
 
@@ -68,23 +89,85 @@ class Game(arcade.Window):
         self.wall_list.draw()
         self.coin_list.draw()
 
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
+                         arcade.csscolor.WHITE, 18)
+
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                self.player_sprite.change_y = constants.PLAYER_JUMP_SPEED
+                arcade.play_sound(self.jump_sound)
         elif key == arcade.key.LEFT or key == arcade.key.A:
-                   self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            if self.player_sprite.left > self.view_left + constants.LEFT_VIEWPORT_MARGIN:
+                self.player_sprite.change_x = -constants.PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-                   self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+            self.player_sprite.change_x = constants.PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.A:
-                   self.player_sprite.change_x = 0
+            self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
-                   self.player_sprite.change_x = 0
+            self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
         self.physics_engine.update()
+
+        # See if we hit any coins
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.coin_list)
+
+        # Loop through each coin we hit (if any) and remove it
+        for coin in coin_hit_list:
+            # Remove the coin
+            coin.remove_from_sprite_lists()
+            # Play a sound
+            arcade.play_sound(self.collect_coin_sound)
+            # Add one to the score
+            self.score += 1
+
+        # --- Manage Scrolling ---
+
+        # Track if we need to change the viewport
+
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + constants.LEFT_VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+
+        # Scroll right
+        right_boundary = self.view_left + constants.SCREEN_WIDTH - constants.RIGHT_VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + constants.SCREEN_HEIGHT - constants.TOP_VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + constants.BOTTOM_VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed = True
+
+        if changed:
+            # Only scroll to integers. Otherwise we end up with pixels that
+            # don't line up on the screen
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+
+            # Do the scrolling
+            arcade.set_viewport(self.view_left,
+                                constants.SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                constants.SCREEN_HEIGHT + self.view_bottom)
 
 
 def main():
