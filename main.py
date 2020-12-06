a9692fff8090f3384fe574c69964b7d3bf669611
python-arcade <waatuurawwaa@gmail.com>
2020-12-06 18:06:31 +0100

    Fully projected level. Implemented character death. Added background.

--- main.py
+++ main.py
@@ -30,54 +30,69 @@ class Game(arcade.Window):
         # Keep track of the score
         self.score = 0
 
+        # Where is the right edge of the map?
+        self.end_of_map = 0
+
+        # Level
+        self.level = 1
+
         # Load sounds
         self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
         self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+        self.game_over = arcade.load_sound(":resources:sounds/gameover1.wav")
 
         arcade.set_background_color(arcade.csscolor.BLACK)
 
-    def setup(self):
-        self.view_bottom = 0
-        self.view_left = 0
-
-        # Keep track of the score
-        self.score = 0
+    def setup(self, level):
 
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+        self.background_list = arcade.SpriteList(use_spatial_hash=True)
 
         image_source = "images/character/GraveRobber.png"
         self.player_sprite = arcade.Sprite(image_source, constants.CHARACTER_SCALING)
-        self.player_sprite.center_x = 24
-        self.player_sprite.center_y = 48
+        self.player_sprite.center_x = constants.PLAYER_START_X
+        self.player_sprite.center_y = constants.PLAYER_START_Y
         self.player_list.append(self.player_sprite)
 
-        for x in range(0, 1250, 24):
-            wall = arcade.Sprite("images/tiles/tile22.png", constants.TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 12
-            self.wall_list.append(wall)
+        # --- Load in a map from the tiled editor ---
 
-        coordinate_list = [[128, 31],
-                           [256, 31],
-                           [384, 31],
-                           [512, 31],
-                           [640, 31],
-                           [768, 31]]
+        # Name of map file to load
+        map_name = "maps/map_castle.tmx"
+        # Name of the layer in the file that has our platforms/walls
+        platforms_layer_name = 'Platforms'
+        # Name of the layer that has items for pick-up
+        coins_layer_name = 'Coins'
+        # Name of the layer that has items for background
+        background_layer_name = 'Background'
 
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite("images/Objects/barrel.png", constants.TILE_SCALING)
-            wall.position = coordinate
-            self.wall_list.append(wall)
+        # Read in the tiled map
+        my_map = arcade.tilemap.read_tmx(map_name)
 
-        # Use a loop to place some coins for our character to pick up
-        for x in range(128, 1250, 256):
-            coin = arcade.Sprite(":resources:images/items/coinGold.png", constants.COIN_SCALING)
-            coin.center_x = x
-            coin.center_y = 96
-            self.coin_list.append(coin)
+        # Calculate the right edge of the my_map in pixels
+        self.end_of_map = my_map.map_size.width * constants.GRID_PIXEL_SIZE
 
+        # -- Background
+        self.background_list = arcade.tilemap.process_layer(my_map,
+                                                            background_layer_name,
+                                                            constants.TILE_SCALING)
+
+        # -- Platforms
+        self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name=platforms_layer_name,
+                                                      scaling=constants.TILE_SCALING,
+                                                      use_spatial_hash=True)
+
+        # -- Coins
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, constants.TILE_SCALING)
+
+        # --- Other stuff
+        # Set the background color
+        if my_map.background_color:
+            arcade.set_background_color(my_map.background_color)
+
+        # Create the 'physics engine'
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
                                                              self.wall_list,
                                                              constants.GRAVITY)
@@ -85,9 +100,10 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
-        self.player_list.draw()
+        self.background_list.draw()
         self.wall_list.draw()
         self.coin_list.draw()
+        self.player_list.draw()
 
         # Draw our score on the screen, scrolling it with the viewport
         score_text = f"Score: {self.score}"
@@ -126,6 +142,30 @@ class Game(arcade.Window):
             arcade.play_sound(self.collect_coin_sound)
             # Add one to the score
             self.score += 1
+
+        # Track if we need to change the viewport
+        changed_viewport = False
+
+        # Did the player fall off the map?
+        if self.player_sprite.center_y < -100:
+            self.player_sprite.center_x = constants.PLAYER_START_X
+            self.player_sprite.center_y = constants.PLAYER_START_Y
+            # Set the camera to the start
+            self.view_left = 0
+            self.view_bottom = 0
+            changed_viewport = True
+            arcade.play_sound(self.game_over)
+
+        # See if the user got to the end of the level
+        if self.player_sprite.center_x >= self.end_of_map:
+            # Advance to the next level
+            self.level += 1
+            # Load the next level
+            self.setup(self.level)
+            # Set the camera to the start
+            self.view_left = 0
+            self.view_bottom = 0
+            changed_viewport = True
 
         # --- Manage Scrolling ---
 
@@ -172,7 +212,7 @@ class Game(arcade.Window):
 
 def main():
     window = Game()
-    window.setup()
+    window.setup(window.level)
     arcade.run()
 
 
