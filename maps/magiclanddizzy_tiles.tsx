248176bf3afd7e4ab7bccc4de4939179598f6eda
python-arcade <waatuurawwaa@gmail.com>
2020-12-29 16:34:34 +0100

    Added tileset.

---
+++ maps/magiclanddizzy_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="magiclanddizzy_tiles" tilewidth="16" tileheight="16" spacing="2" tilecount="1056" columns="44">
+ <image source="magiclanddizzy_tiles.gif" width="790" height="430"/>
+</tileset>
